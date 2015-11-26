d92d8f4d5aa9b7c5669ffb26d6f0606fe381255a
Duy Trí <duytri2712@gmail.com>
2015-11-26 15:48:00 +0700

    Solved some problem in multi-dimentional array

--- BaiTapViDuScala/src/uit/islab/multiDementionalArray/MultiDementionalArray.scala
+++ BaiTapViDuScala/src/uit/islab/multiDementionalArray/MultiDementionalArray.scala
@@ -1,29 +1,36 @@
 package uit.islab.multiDementionalArray
 
 import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.ListBuffer
 
 object MultiDementionalArray {
   def run(): Unit = {
-    println("~~~ Chuong trinh thao tac voi mang 2 chieu")
-    println("Nhap vao so dong cua mang rows = ")
+    println("~~~ Chuong trinh thao tac tren mang 2 chieu voi so cot chua xac dinh")
+    print("Nhap vao so dong cua mang rows = ")
     val rows = readInt()
-    println("Nhap vao so cot cua mang cols = ")
-    val cols = readInt()
-    var array = Array.ofDim[Int](rows, cols)
-    var array2 = new Array[ArrayBuffer[String]](rows)
+    var array2 = new Array[Array[String]](rows)
     for (i <- 0 to (rows - 1) if i % 2 == 0) {
-      //array2(i).append("aaaa")
-      //array2(i).append("bbb")
+      var arrTmp = new ArrayBuffer[String]
+      arrTmp += "aaaa"
+      arrTmp += "bbb"
+      arrTmp += "ccccccc"
+      array2.update(i, arrTmp.toArray)
     }
     for (i <- 0 to (rows - 1) if i % 2 == 1) {
-      //array2(i).append("111")
-      //array2(i).append("222")
-      //array2(i).append("333")
+      var arrTmp = new ArrayBuffer[String]
+      arrTmp += "111"
+      arrTmp += "222"
+      arrTmp += "333"
+      arrTmp += "444"
+      arrTmp += "55555"
+      array2.update(i, arrTmp.toArray)
     }
-    /*for (i <- 0 to (array2.length - 1)) {
+
+    for (i <- 0 to (array2.length - 1)) {
       for (j <- 0 to (array2(i).length - 1)) {
-        println(array2(i)(j));
+        print(array2(i)(j) + "\t")
       }
-    }*/
+      println()
+    }
   }
 }
