9965fffde40ef23aab1f407f5c47dbd8dfd7d18d
Duy Trí <duytri2712@gmail.com>
2015-12-22 16:47:43 +0700

    Fix relative path of RW Text file

--- BaiTapViDuScala/src/uit/islab/fileRW/RWTextFile.scala
+++ BaiTapViDuScala/src/uit/islab/fileRW/RWTextFile.scala
@@ -2,11 +2,12 @@ package uit.islab.fileRW
 
 import scala.collection.mutable.ArrayBuffer
 import scala.io.Source
+import java.io.File
 
 object RWTextFile {
   def run(): Unit = {
     println("~~~ Chuong trinh thao tac tren file TXT ~~~")
-    val filePath = "C:\\Users\\duytr\\Desktop\\vietname-stopwords-dash.txt"
+    val filePath = "./libs/vnstopword.txt"
     var arrText = new ArrayBuffer[String]
     Source.fromFile(filePath, "utf-8").getLines().foreach { x => arrText.append(x) }
     arrText.foreach { println }
